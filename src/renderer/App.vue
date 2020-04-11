d4de748e8942b9b48485586f730f7200ee75cf76
Mike Rodrigues <rodrigues.mi@husky.neu.edu>
2020-04-11 14:41:57 -0400

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'kinvey-console',
+  };
+</script>
+
+<style>
+  /* CSS */
+</style>
